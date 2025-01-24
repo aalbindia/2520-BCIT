7e422778866837786b3d6bab7dfd85024f9ae69e
allen <aalbindia@my.bcit.ca>
2025-01-23 21:38:57 -0800

    With all Asynchronous functions

--- main.js
+++ main.js
@@ -10,34 +10,36 @@ function processInput() {
     const folderName = './dataPoints';
     const filePath = folderName + '/points.txt';
     const content = numbers.join('\n');
-    const [x_2,x_1,y_2,y_1] = numbers;
+    const [x_2,x_1,y_2,y_1] = numbers; // We assign each element in the array a given name, this allows to call it in our distance fn
     const calculatedDistance = distance(x_2, x_1, y_2, y_1);
     const appendContent = `\nThe distance between your two points: (${x_1}, ${y_1}), (${x_2}, ${y_2}) is ${calculatedDistance}\n`;
     
 
 
-    if (!fs.existsSync(folderName)) {
-        fs.mkdir(folderName, {recursive: true}, (err) => {
-            if (err) {
-                throw err;
-                
-            }
+    fs.mkdir(folderName, {recursive: true}, (err) => {
+        if (err) {
+            throw err('Folder has already been made');
+            
+        }
 
-        });
+    });
+        
 
         
-        fs.writeFile(filePath, content, (err) => {
-            if (err) throw err;
+    fs.writeFile(filePath, content, (err) => {
+            if (err) throw err('File has already been made');
             console.log('Content Saved')
         });
+        fs.appendFile(filePath, appendContent, (err) => {
+            if (err) throw err;
+            console.log('Distance has been added.')
+        } )
+    
     }
-    fs.appendFile(filePath, appendContent, (err) => {
-        if (err) throw err;
-        console.log('Distance has been added.')
-    } )
+
+    
 
 
-}
 processInput();    
 
 
